fa0d42eb86d2275850cafe873314c379ee3a97f3
snehaa1989 <61073116+snehaa1989@users.noreply.github.com>
2020-11-29 11:32:20 +0530

    Update reusableCard1.dart

--- pcod_helper/lib/components/reusableCard1.dart
+++ pcod_helper/lib/components/reusableCard1.dart
@@ -16,7 +16,7 @@ class ReusableCard1 extends StatelessWidget {
         child: GestureDetector(
           onTap: () {
             Navigator.push(context,
-              MaterialPageRoute(builder: (context) =>Scaffold()),
+              MaterialPageRoute(builder: (context) =>Scaffold()), // RETURN SCAFFOLD UNTIL WE HAVE DIET DART FILE
             );
           },
           child: Card(
